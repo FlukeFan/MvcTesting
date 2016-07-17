bee699cdc6f7a742c883719ab04beb43b3db8cb0
Richard Brown <flukefan@googlemail.com>
2016-07-17 16:47:44 +0100

    Initial passing of tests ported from Lucid.

---
+++ MvcTesting.StubApp/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcTesting.StubApp
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RegisterRoutes(RouteTable.Routes);
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
