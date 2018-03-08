302e056efccfbadf3e7f6acd5c9219a1518d46eb
Richard Brown <fluke_fan@hotmail.com>
2018-03-08 07:41:57 +0000

    Added form for testing submit.

--- MvcTesting.StubApp/Controllers/StubController.cs
+++ MvcTesting.StubApp/Controllers/StubController.cs
@@ -1,9 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcTesting.StubApp.Views.Stub;
 
 namespace MvcTesting.StubApp.Controllers
 {
     public class StubController : Controller
     {
+        [HttpGet]
+        public IActionResult SimpleForm()
+        {
+            var model = new SimpleFormModel
+            {
+                Id = 123,
+                Text = "existing",
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult SimpleForm(SimpleFormModel model)
+        {
+            return Content("Not implemented yet");
+        }
+
         public IActionResult ViewRequest()
         {
             var model = new ViewRequestModel
---
+++ MvcTesting.StubApp/Views/Stub/SimpleFormModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MvcTesting.StubApp.Views.Stub
+{
+    public class SimpleFormModel
+    {
+        public int      Id      { get; set; }
+        public string   Text    { get; set; }
+    }
+}
